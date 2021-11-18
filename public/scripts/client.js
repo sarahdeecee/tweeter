a181d37dd5817002b09bef7380012459c052c72d
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2021-11-17 21:15:09 -0500

    loads new tweets without refreshing page. newest tweets at top

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,70 +3,86 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
 
-const renderTweets = tweets => {
-  for (let tweet of tweets) {
-    const $tweet = createTweetElement(tweet);
-    $('#tweets-container').append($tweet);
-  }
-}
 
-const createTweetElement = tweetObj => {
-  const createdTime = timeago.format(tweetObj.created_at);
-  const tweetPage = $(`
-    <article>
-      <header>
-        <span class="username"><img class="profileimg" src="${tweetObj.user.avatars}">${tweetObj.user.name}</span>
-        <span class="userid">${tweetObj.user.handle}</span>
-      </header>
-      <p>${tweetObj.content.text}</p>
-      <footer>
-        <span>${createdTime}</span>
-        <span class="icons"><i class="fas fa-flag"></i> <i class="fas fa-retweet"></i> <i class="fas fa-heart"></i></span>
-      </footer>
-    </article>
-  `);
-  return tweetPage;
-};
-
-const addNewTweet = event => {
-
-};
 
 $(document).ready(function() {
-  renderTweets(data);  
-  $("#posttweet").submit(function( event ) {
+
+  //Render Tweets Function Starts here..
+  const renderTweets = tweets => {
+    for (let tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').prepend($tweet);
+    }
+  };
+
+  //Create Tweet Function to Create dynamically html elements  
+  const createTweetElement = function(tweetObj) {
+    const avatar = tweetObj.user.avatars;
+    const username = tweetObj.user.name;
+    const handle = tweetObj.user.handle;
+    const text = tweetObj.content.text;
+    const createdTime = timeago.format(tweetObj.created_at);;
+    if (typeof tweetObj !== 'object') {
+      console.log('not an object',tweetObj);
+    }
+    const tweetPage = $(`
+      <article>
+        <header>
+          <span class="username"><img class="profileimg" src="${avatar}">${username}</span>
+          <span class="userid">${handle}</span>
+        </header>
+        <p>${text}</p>
+        <footer>
+          <span>${createdTime}</span>
+          <span class="icons"><i class="fas fa-flag"></i> <i class="fas fa-retweet"></i> <i class="fas fa-heart"></i></span>
+        </footer>
+      </article>
+    `);
+    return tweetPage;
+  };
+  
+  const loadTweets = () => {
+    //make a request to /tweets
+    //receive array of tweets as JSON
+    $.ajax('/tweets', { method: 'GET' })
+    .then(renderTweets);
+  };
+
+  loadTweets();
+  
+  $("#posttweet").submit(function(event) {
+    const charCount = $(event.target.text).serialize().length - 5;
     event.preventDefault();
-    console.log("submit pressed!");
-    const $tweetText = $(event.target.text).serialize();
-    console.log("VAL--->", $tweetText); //text=test
-    $.post('/tweets/', $tweetText).then(() => {
-      console.log("post to tweets", $tweetText);
-    })
-    $('#tweet-text').val('');
+    if (!charCount)  {
+      alert("Please enter something");
+      return;
+    } else if (charCount > 140) {
+      alert("Please shorten your thoughts to 140 characters or less!");
+      return;
+    }
+    addNewTweet(event);
+    $(this).find(".counter").text(140);
   });
+  //Create new Tweet that is called within the Submit Button
+  const addNewTweet = function(event) {
+    const $tweetText = $(event.target.text).serialize();
+    $.post('/tweets', $tweetText).then(() => {
+      console.log("post to tweets", $tweetText);
+      // $('#tweets-container').prepend(createTweetElement($tweetText));
+      $('#tweet-text').val(''); //clears textarea
+      $('#tweets-container').empty();
+      loadTweets();
+    })
+  };
+
+  // $("#writenew").click(function() {
+    // const newTweetPos = $("[name='" + $(this).attr('href').replace("#", '') + "']").position();
+    // const tweetsTop = $('article').first().scrollTop();
+    // console.log(`new: ${newTweetPos} tweets: ${tweetsTop}`);
+  //   window.scroll({
+  //     top: 0,
+  //     behavior: "smooth"
+  //   });
+  // });
 });
--- public/scripts/composer-char-count.js
+++ public/scripts/composer-char-count.js
@@ -1,27 +1,3 @@
-
-document.addEventListener('DOMContentLoaded', function(event) {
-  // console.log('The DOM is loaded!');
-});
-
-const alertMe = () => {
-  alert("Hello!");
-  // $("#blackBox").hide();
-};
-
-const checkChar = function() {
-  const textboxInput = this;
-  console.log(this);
-  console.log(textboxInput.value);
-}
-const checkCharLength = function() {
-  const charLimit = 140;
-  const textboxLength = this.value.length;
-  const charLeft = charLimit - textboxLength;
-  $('counter')
-  console.log(charLeft);
-}
-
-
 $(document).ready(function() {
   const charLimit = 140;
   $('#tweet-text').on('input', function(event) {
